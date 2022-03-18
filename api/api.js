64e01967a60e6ebef993f8f921f6440fd6c88956
Wagner Ghedin <wagnerghedin10@gmail.com>
2022-03-17 21:28:57 -0300

    Add files via upload

--- api/api.js
+++ api/api.js
@@ -1,16 +1,26 @@
-function teste(){
-    var select = document.getElementsByClassName("marca")
+onload = ()=>{
+    const select = document.querySelector("#select_marca")
+
     const url = "https://parallelum.com.br/fipe/api/v1/carros/marcas"
     var request = fetch(url)
-    var data = request.then(function(response){
+    var data = request.then(function (response) {
         return response.json()
     })
-    
-    data.then(function(dado){
-        console.log(dado)
-        for(i =0; i< dado.length; i++){
-            var marca = dado[i].nome
-            select.append(marca)
+
+    data.then(function (dados) {
+        console.log(dados)
+        select.innerHTML = ""
+        // for (i = 0; i < dado.length; i++) {
+        //     const option = document.createElement("option")
+        //     option.text = dado[i].nome
+        //     option.value = dado[i].codigo
+        //     select.appendChild(option)
+        // }
+        for(const dado of dados){
+            const option = document.createElement("option")
+            option.text = dado.nome
+            option.value = dado.codigo
+            select.appendChild(option)
         }
     })
 }
