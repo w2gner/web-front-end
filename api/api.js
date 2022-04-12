3130441a732be059d697ad0c09a158d567a2f2de
Wagner <wagnerghedin10@gmail.com>
2022-04-11 22:14:17 -0300

    Criada rota para retornar imagem do herói

--- api/api.js
+++ api/api.js
@@ -6,28 +6,28 @@ let app = express();
 app.get('/heroes', function (req, res, next) {
     let request = fetch("https://akabab.github.io/superhero-api/api/all.json")
     let heroName = req.query.name;
+    let found = false;
 
     request.then(function (response) {
         if (heroName) {
             return response.json().then(function (data) {
                 let hero = data.find(function (hero) {
-                    return hero.name === heroName;
+                    if (hero.name === heroName && !found) {
+                        res.json(hero);
+                        found = true;
+                        request.finally(console.log("Herói encontrado"));
+                    }
                 });
-                if (hero) {
-                    res.json(hero);
-                    next();
-                } else {
+                if (hero == undefined && !found) {
                     res.status(404).json({
                         error: "Herói não encontrado"
                     });
-                    next();
                 }
             });
         }
         else {
             return response.json().then(function (data) {
                 res.json(data);
-                next();
             });
         }
     })
@@ -36,16 +36,19 @@ app.get('/heroes', function (req, res, next) {
 app.get('/occupation', function (req, res, next) {
     let request = fetch("https://akabab.github.io/superhero-api/api/all.json")
     let heroName = req.query.name;
+    let found = false;
 
     request.then(function (response) {
         if (heroName) {
             return response.json().then(function (data) {
                 let hero = data.find(function (hero) {
-                    if (hero.name === heroName) {
+                    if (hero.name === heroName && !found) {
                         res.json(hero.work.occupation);
+                        found = true;
+                        request.finally(console.log("Herói encontrado"));
                     }
                 });
-                if (!hero == undefined) {
+                if (hero == undefined && !found) {
                     res.status(404).json({
                         error: "Herói não encontrado"
                     });
@@ -55,6 +58,40 @@ app.get('/occupation', function (req, res, next) {
         else {
             return response.json().then(function (data) {
                 res.json(data);
+            });
+        }
+    })
+});
+
+app.get('/images', function (req, res, next) {
+    let request = fetch("https://akabab.github.io/superhero-api/api/all.json")
+    let heroName = req.query.name;
+    let found = false;
+
+    request.then(function (data) {
+        if (heroName) {
+            return data.json().then(function (data) {
+                let hero = data.find(function (hero) {
+                    if (hero.name === heroName && !found) {
+                        res.json(hero.images.lg);
+                        found = true;
+                        //buscar uma forma de encerrar a promise do request
+                        request.finally(console.log("Herói encontrado"));
+                    }
+                });
+                if (hero == undefined && !found) {
+                    res.status(404).json({
+                        error: "Herói não encontrado"
+                    });
+                }
+            });
+        } else if (heroName != undefined) {
+            res.status(404).json({
+                error: "Herói não encontrado"
+            });
+        } else {
+            res.status(404).json({
+                error: "Envie o parâmetro 'name'"
             });
         }
     })
